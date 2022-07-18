8228d49aadf4996fb7df293288e753749e145e14
Daniel Rosenberg <daniel.rosenberg333@gmail.com>
2022-07-18 14:53:05 +0800

    path fix 2.6

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const publicPath = path.join(__dirname, "client", "public");
+const buildPath = path.join(__dirname, "client/build");
 const dotenv = require("dotenv");
 dotenv.config();
 const port = process.env.PORT || 5000;
 
-app.use(express.static("client/public"));
+app.use(express.static(buildPath));
 console.log(publicPath);
+console.log(buildPath);
 app.get("*", (req, res) => {
   try {
     res.sendFile(path.join(publicPath, "index.html"));
