27d5570916dd03f1e6117a7b348a5bf070c44b2d
Daniel Rosenberg <daniel.rosenberg333@gmail.com>
2022-07-18 14:18:16 +0800

    path fix

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-const publicPath = path.join(__dirname, "..", "public");
+const publicPath = path.join(__dirname, "client", "public");
 const dotenv = require("dotenv");
 dotenv.config();
 const port = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("public"));
-  console.log(publicPath);
-  app.get("*", (req, res) => {
+app.use(express.static("public"));
+console.log(publicPath);
+app.get("*", (req, res) => {
+  try {
     res.sendFile(path.join(publicPath, "index.html"));
-  });
-}
+    console.log("Sent:", path);
+  } catch (error) {
+    console.log(error);
+    return 1;
+  }
+});
 
 app.listen(port, (err) => {
   if (err) return console.log(err);
