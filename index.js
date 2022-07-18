956d56fcbd1295ec5428368f2df7395049ea6b99
Daniel Rosenberg <daniel.rosenberg333@gmail.com>
2022-07-18 15:04:29 +0800

    path fix 2.9

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const publicPath = path.join(__dirname, "client", "public");
-const buildPath = path.join(__dirname, "client/build");
+const buildPath = path.join(__dirname, "./client/build");
 const dotenv = require("dotenv");
 dotenv.config();
 const port = process.env.PORT || 5000;
