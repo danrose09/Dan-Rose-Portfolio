da6ee41eb95c6c8d6b0a39a16743edd5d58a4bd3
Daniel Rosenberg <daniel.rosenberg333@gmail.com>
2022-07-18 15:31:53 +0800

    path fix 2.12

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const path = require("path");
 const express = require("express");
+const path = require("path");
 const app = express();
 const publicPath = path.join(__dirname, "client", "public");
 const buildPath = path.join(__dirname, "./client/build");
@@ -7,7 +7,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 const port = process.env.PORT || 5000;
 
-app.use(express.static(buildPath));
+app.use(express.static("client/build"));
 console.log(publicPath);
 console.log(buildPath);
 app.get("*", (req, res) => {
